b5c7c1f13de31db695cf2fab6ae3550f2fd9d77a
Kaj-Michael Lang <milang@tal.org>
2017-02-23 23:48:43 +0200

    Fix sandbox typo

--- src/main.cpp
+++ src/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
     user.insert("username", settings.getSettingsStr("username", ""));
     user.insert("password", settings.getSettingsStr("password", ""));
     user.insert("apikey", API_KEY);
-    user.insert("urlSanbox", API_SERVER_SANDBOX);
+    user.insert("urlSandbox", API_SERVER_SANDBOX);
     user.insert("urlProduction", API_SERVER_PRODUCTION);
 
     QString locale(QLocale().name());
