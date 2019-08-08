9c363094961180ba6894fdaae910204d32fc9627
Kaj-Michael Lang <milang@tal.org>
2019-08-08 15:59:19 +0300

    Simplify, use Q_PROPERTY MEMBER

--- src/coloritem.h
+++ src/coloritem.h
@@ -7,29 +7,13 @@
 class ColorItem : public QObject
 {
     Q_OBJECT
-
-    Q_PROPERTY(QString cid READ cid NOTIFY cidChanged)
-    Q_PROPERTY(QString color READ color NOTIFY colorChanged)
-    Q_PROPERTY(QString code READ code NOTIFY codeChanged)
+    Q_PROPERTY(QString cid MEMBER m_cid NOTIFY cidChanged)
+    Q_PROPERTY(QString color MEMBER m_color NOTIFY colorChanged)
+    Q_PROPERTY(QString code MEMBER m_code NOTIFY codeChanged)
 
 public:
     explicit ColorItem(QObject *parent = nullptr);
     explicit ColorItem(const QString &cid, const QString &color, const QString &code, QObject *parent = nullptr);
-
-    QString cid() const
-    {
-        return m_cid;
-    }
-
-    QString color() const
-    {
-        return m_color;
-    }
-
-    QString code() const
-    {
-        return m_code;
-    }
 
     ColorItem *fromVariantMap(const QVariantMap &data, QObject *parent = nullptr);
 
