ddaeee377694181eca5d7cfb0a45721514f08f02
Kaj-Michael Lang <milang@tal.org>
2019-11-20 13:36:14 +0200

    Allow both 8 and 13 EANs

--- src/eanvalidator.cpp
+++ src/eanvalidator.cpp
@@ -10,29 +10,45 @@ EANValidator::EANValidator(QObject *parent) : QValidator(parent)
 QValidator::State EANValidator::validate(QString &code, int &pos) const
 {
     Q_UNUSED(pos)
-    int val=0;
+    int val=0,eanl=0;
 
-    qDebug() << code << pos;
+    if (code.length()==0)
+        return QValidator::Intermediate;
 
-    if (code.length()<13)
+    if (code.at(pos-1).isDigit()==false)
+        return QValidator::Invalid;
+
+    if ((code.length()<8 || code.length()>8) && code.length()<13)
         return QValidator::Intermediate;
 
     if (code.length()>13)
         return QValidator::Invalid;
 
-    for (int i=0;i<12;i++) {
+    if (code.length()==8)
+        eanl=7;
+    else if (code.length()==13)
+        eanl=12;
+    else
+        return QValidator::Intermediate;
+
+    qDebug() << code << code.length() << pos << eanl;
+
+    for (int i=0;i<eanl;i++) {
         const QChar c=code.at(i);
-        if (c.isDigit()==false)
-            return QValidator::Invalid;        
+        if (c.isDigit()==false) {
+            pos=i;
+            return QValidator::Invalid;
+        }
 
         val+=c.digitValue()*((i % 2==0) ? 1 : 3);
     }
     int cd=(10-(val % 10)) % 10;
 
-    return code.at(12).digitValue()==cd ? QValidator::Acceptable : QValidator::Invalid;
+    return code.at(eanl).digitValue()==cd ? QValidator::Acceptable : QValidator::Intermediate; // (eanl==12 ? QValidator::Invalid : QValidator::Intermediate);
 }
 
 void EANValidator::fixup(QString &code) const
 {
     code=code.trimmed();
+    code.truncate(13);
 }
